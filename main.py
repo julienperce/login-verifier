e006a8fface31dcf8530cb71e8f1f6a56104ed36
julienperce <58319391+julienperce@users.noreply.github.com>
2020-10-06 21:43:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-registeredLogins = {}
-a = int(input("Select your action: 1 to register; 2 to login"))
-if a == 1 or a == 2:
-    newRegUser = input("Choose your username: ")
-    newRegPass = input("Choose your password: ")
-    registeredLogins[newRegUser] = newRegPass
-else: 
-    print("wrong input")
 
-print(registeredLogins)
+entriesFile = open("entries.txt", "r+")
+
+a = int(input("Select your action: 1 to register; 2 to login"))
+
+if a == 1:
+    regUser = input("Choose your username: ")
+    regPass = input("Choose your password: ")
+    entriesFile.write(regUser)
+    entriesFile.write(":")
+    entriesFile.write(regPass)
+elif a == 2:
+    loginUser = input("Enter username: ")
+    loginPass = input("Enter password: ")
+    for line in entriesFile:
+        valueSplit = line.split(":")
+        loggedUser = valueSplit[0]
+        loggedPass = valueSplit[1]
+    if loginUser == loggedUser and loginPass == loggedPass:
+        print("succesful")
+    else:
+        print("failure")
+else:
+    print("wrong value")
