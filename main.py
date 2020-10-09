0b13b0a23fb7a4bc9eb595a5779cfa8d3490d25f
julienperce <58319391+julienperce@users.noreply.github.com>
2020-10-08 21:05:57 -0500

    Clean up, add functions and proper sys.exit

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
+import os
+import sys
 
+readEntries = open("entries.txt")
 entriesFile = open("entries.txt", "r+")
 
+def amtRegUsers():
+    numLines = 0
+    return len(open("entries.txt").readlines())
+print(f"there are currently {amtRegUsers()} users registered.")
 a = int(input("Select your action: 1 to register; 2 to login"))
 
-if a == 1:
+
+def regNewUser():
     regUser = input("Choose your username: ")
     regPass = input("Choose your password: ")
     entriesFile.write(regUser)
     entriesFile.write(":")
     entriesFile.write(regPass)
-elif a == 2:
+def loginExistingUser():
     loginUser = input("Enter username: ")
     loginPass = input("Enter password: ")
     for line in entriesFile:
         valueSplit = line.split(":")
         loggedUser = valueSplit[0]
         loggedPass = valueSplit[1]
-    if loginUser == loggedUser and loginPass == loggedPass:
-        print("succesful")
-    else:
-        print("failure")
+        if loginUser == loggedUser and loginPass == loggedPass:
+            print("succesful")
+        else:
+            print("failure")
+
+if a == 1:
+    regNewUser()
+
+elif a == 2:
+    if os.stat("entries.txt").st_size == 0:
+        print("Error: there are no registered users")
+        sys.exit(0) # we pass the arg 0 to make sure it is a "successful termination"; and to not raise an exception
+    loginExistingUser()
 else:
     print("wrong value")
+    sys.exit(0)
