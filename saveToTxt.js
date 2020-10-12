e3400a6dd5a3f0db60b61e23abe066ae111f6327
julienperce <58319391+julienperce@users.noreply.github.com>
2020-10-11 22:49:19 -0500

    tabs -> spaces

--- saveToTxt.js
+++ saveToTxt.js
@@ -1,4 +1,4 @@
 function writeToEntries(){
-	var inputUser = document.getElementById("username").value;
-	var inputPass = document.getElementById("password").value;
+    var inputUser = document.getElementById("username").value;
+    var inputPass = document.getElementById("password").value;
 }
