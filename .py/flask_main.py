1b5e65d880e5e037d37e91cc8caf17536fb2ad73
julienperce <58319391+julienperce@users.noreply.github.com>
2020-10-15 23:09:46 -0500

    trying to fix Method not Allowed error

--- .py/flask_main.py
+++ .py/flask_main.py
@@ -1,4 +1,4 @@
-from flask import Flask; from flask import render_template
+from flask import Flask; from flask import render_template, request
 
 app = Flask(__name__, template_folder="../templates") # we put 2 dots to search in one directory before /templates
 
@@ -6,9 +6,13 @@ app = Flask(__name__, template_folder="../templates") # we put 2 dots to search 
 def homepage():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def loginPage():
     return render_template("login.html")
+
+@app.route("/login/page2")
+def page2():
+    return render_template("page2.html")
 
 if __name__ == "__main__":
     app.run()
